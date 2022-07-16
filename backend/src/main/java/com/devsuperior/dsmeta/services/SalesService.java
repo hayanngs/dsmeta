c705ff192c80c2c64eb2ddfd76be7bc9e3ceb7fa
hayanngs <hayannow@gmail.com>
2022-07-15 21:42:58 -0300

    Date select

--- backend/src/main/java/com/devsuperior/dsmeta/services/SalesService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SalesService.java
@@ -3,17 +3,36 @@ package com.devsuperior.dsmeta.services;
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.repositories.SaleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.List;
 
 @Service
 public class SalesService {
 
 	@Autowired
-	private SaleRepository repository;
+	private SaleRepository saleRepository;
 
-	public List<Sale> findSales() {
-		return repository.findAll();
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+		//LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		LocalDate min;
+		LocalDate max;
+		try {
+			min = LocalDate.parse(minDate);
+		} catch (Exception e) {
+			min = LocalDate.parse("1900-01-01");
+		}
+		try {
+			max = LocalDate.parse(maxDate);
+		} catch (Exception e) {
+			max = LocalDate.parse("2050-01-01");
+		}
+		Page<Sale> salePage = saleRepository.findSales(min, max, pageable);
+		return salePage;
 	}
 }
